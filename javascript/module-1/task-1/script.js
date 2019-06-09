30034718dd86c689c63fcf3613a939cc3ac77a48
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-06-09 14:12:11 +0300

    code fixes

--- javascript/module-1/task-1/script.js
+++ javascript/module-1/task-1/script.js
@@ -1,25 +1,28 @@
 'use strict'
 const ADMIN_PASSWORD = 'm4ng0h4ckz';
 const userInput = prompt('Введите пароль');
-let message = '';
-
 
 if (userInput === null) {
-  message = 'Отменено пользователем!';
-  alert(message);
+  alert('Отменено пользователем!');
+} else if (userInput === '') {
+  alert('Пароль не был введен!');
 } else if (userInput === ADMIN_PASSWORD) {
-  message = 'Добро пожаловать!';
-  alert(message);
+  alert('Добро пожаловать!');
 } else {
-  message = 'Доступ запрещен, неверный пароль!';
-  alert(message);
+  alert('Доступ запрещен, неверный пароль!');
 }
 
+/*
+То же самое тернарным оператором
+*/
 
-// То же самое тернарным опреатором
+// const ADMIN_PASSWORD = 'm4ng0h4ckz';
+// const userInput = prompt('Введите пароль');
+// let message = '';
 
-
-// message = userInput === null ? 'Отменено пользователем!' :
-//           userInput === ADMIN_PASSWORD ? 'Добро пожаловать!' :
+// message = userInput === null ? 'Отменено пользователем!':
+//           userInput === '' ? 'Пароль не был введен!':
+//           userInput === ADMIN_PASSWORD ? 'Добро пожаловать!':
 //           'Доступ запрещен, неверный пароль!';
+
 // alert(message);
