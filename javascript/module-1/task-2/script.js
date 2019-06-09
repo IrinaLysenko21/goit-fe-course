ebb303dcd9e3a44dd0dd2aefd858cb8083d15591
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-06-09 13:39:54 +0300

    task-3 is done

--- javascript/module-1/task-2/script.js
+++ javascript/module-1/task-2/script.js
@@ -4,23 +4,42 @@ const pricePerDroid = 3000;
 let quantity = Number(prompt('Какое количество дроидов вы хотите приобрести?'));
 const totalPrice = quantity * pricePerDroid;
 const balance = credits - quantity * pricePerDroid;
-let message = '';
 
 if (quantity === 0) {
-  message = 'Отменено пользователем!';
-  alert(message);
-} else if (totalPrice > credits) {
-  message = 'Недостаточно средств на счету!';
-  alert(message);
-} else if (totalPrice <= credits) {
-  message = `Вы купили ${quantity} дроидов, на счету осталось ${balance} кредитов.`;
-  alert(message);
+  alert('Отменено пользователем!');
 } else {
-  message = 'Введите число!';
-  alert(message);
+
+  switch (true) {
+    case totalPrice > credits:
+      alert('Недостаточно средств на счету!');
+      break;
+
+    case totalPrice <= credits:
+      alert(`Вы купили ${quantity} дроидов, на счету осталось ${balance} кредитов.`);
+      break;
+
+    default:
+      alert('Введите число!');
+  }
 }
 
-// То же самое тернарным опреатором
+/*
+ Без switch
+*/
+
+// if (quantity === 0) {
+//   alert('Отменено пользователем!');
+// } else if (totalPrice > credits) {
+//   alert('Недостаточно средств на счету!');
+// } else if (totalPrice <= credits) {
+//   alert(`Вы купили ${quantity} дроидов, на счету осталось ${balance} кредитов.`);
+// } else {
+//   alert('Введите число!');
+// }
+
+/*
+То же самое тернарным опреатором
+*/
 
 // message = quantity === 0 ? 'Отменено пользователем!':
 //           totalPrice > credits ? 'Недостаточно средств на счету!':
