29c509033dcfefbe27db614089a4d33ba7a6a9d0
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-06-17 02:39:58 +0300

    code fixes

--- javascript/module-2/task-2/script.js
+++ javascript/module-2/task-2/script.js
@@ -12,6 +12,7 @@ if (userInput !== null) {
   } else if (!passwords.includes(userInput)) {
     do {
       warning = prompt(`Неверный пароль, у вас осталось ${attemptsLeft} попыток!`);
+      if (warning === null) break;
       if (!passwords.includes(warning)) {
         while (warning === '') {
           alert('Пароль не был введен!');
@@ -34,3 +35,38 @@ if (userInput !== null) {
 } else {
   alert('Вы нажали отмену!');
 }
+
+// const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+// let attemptsLeft = 3;
+// let warning;
+
+// const userInput = prompt('Введите пароль');
+
+// if (userInput !== null) {
+//   if (userInput === '') {
+//     alert('Пароль не был введен!');
+//   } else if (!passwords.includes(userInput)) {
+//     do {
+//       warning = prompt(`Неверный пароль, у вас осталось ${attemptsLeft} попыток!`);
+//       if (!passwords.includes(warning)) {
+//         while (warning === '') {
+//           alert('Пароль не был введен!');
+//           attemptsLeft += 1;
+//           break;
+//         }
+//         attemptsLeft -= 1;
+//         if (attemptsLeft === 0) {
+//         alert('У вас закончились попытки, аккаунт заблокирован!');
+//         break;
+//         }
+//       } else {
+//         alert('Добро пожаловать!');
+//         break;
+//       }
+//     } while (warning !== null);
+//   } else {
+//     alert('Добро пожаловать!');
+//   }
+// } else {
+//   alert('Вы нажали отмену!');
+// }
