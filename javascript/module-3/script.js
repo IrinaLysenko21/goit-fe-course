3571ba6114dde8dc886b498eb7203420e7e3a816
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-06-22 22:33:31 +0300

    code restructuring

--- javascript/module-3/script.js
+++ javascript/module-3/script.js
@@ -3,6 +3,10 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const userInput = prompt('Введите пароль');
 
+/*
+Обычные функции
+*/
+
 const isLoginValid = function(login) {
   let result;
   if (login.length >= 4 && login.length <= 16) {
@@ -12,12 +16,6 @@ const isLoginValid = function(login) {
   }
   return result;
 };
-
-/*
- Тернарным оператором:
- */
-// const isLoginValid = (login) => login.length >= 4 && login.length <= 16 ? true : false;
-
 
 const isLoginUnique = function(allLogins, login) {
   let result;
@@ -29,10 +27,6 @@ const isLoginUnique = function(allLogins, login) {
   return result;
 };
 
-/*
- Тернарным оператором:
- */
-// const isLoginUnique = (allLogins, login) => !allLogins.includes(login) ? true : false;
 
 const addLogin = function(allLogins, login) {
   let result = isLoginValid(login);
@@ -53,3 +47,29 @@ addLogin(logins, userInput);
 
 console.log(logins);
 
+/*
+ Стрелочные функции + тернарный оператор:
+ */
+
+// const isLoginValid = (login) => login.length >= 4 && login.length <= 16 ? true : false;
+
+// const isLoginUnique = (allLogins, login) => !allLogins.includes(login) ? true : false;
+
+// const addLogin = (allLogins, login) => {
+//   let result = isLoginValid(login);
+//   if (result !== false) {
+//     result = isLoginUnique(allLogins, login);
+//     if (result === true) {
+//       allLogins.push(login);
+//       alert('Логин успешно добавлен!');
+//     } else {
+//       alert('Такой логин уже используется!');
+//     }
+//   } else {
+//     alert('Ошибка! Логин должен быть от 4 до 16 символов');
+//   }
+// };
+
+// addLogin(logins, userInput);
+
+// console.log(logins);
