ad1a69ad8df30ccd6d5e88bf5ae1b60972d374e5
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-07-02 17:20:01 +0300

    changing code of ternary operator

--- javascript/module-3/script.js
+++ javascript/module-3/script.js
@@ -4,45 +4,43 @@ const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const userInput = prompt('Введите пароль');
 
 /*
-Обычные функции
-*/
+ Стрелочные функции + тернарный оператор:
+ */
 
-const isLoginValid = function(login) {
-  return login.length >= 4 && login.length <= 16;
-};
+const isLoginValid = (login) => login.length >= 4 && login.length <= 16;
 
-const isLoginUnique = function(allLogins, login) {
-  return allLogins.includes(login);
-};
+const isLoginUnique = (allLogins, login) => allLogins.includes(login);
 
-const addLogin = function(allLogins, login) {
-  if (isLoginValid(login)) {
-    if(!isLoginUnique(allLogins, login)) {
-      alert('Логин успешно добавлен!');
-      allLogins.push(login);
-    } else {
-      alert('Такой логин уже используется!');
-    }
-  } else {
+const addLogin = (allLogins, login) => {
+    isLoginValid(login) ?
+    (!isLoginUnique(allLogins, login) ? (allLogins.push(login), alert('Логин успешно добавлен!')) :
+    alert('Такой логин уже используется!')) :
     alert('Ошибка! Логин должен быть от 4 до 16 символов');
-  }
-};
+}
 
 addLogin(logins, userInput);
 console.log(logins);
 
 /*
- Стрелочные функции + тернарный оператор:
- */
+Обычные функции
+*/
 
-// const isLoginValid = (login) => login.length >= 4 && login.length <= 16 ? true : false;
+// const isLoginValid = function(login) {
+//   return login.length >= 4 && login.length <= 16;
+// };
 
-// const isLoginUnique = (allLogins, login) => allLogins.includes(login) ? true : false;
+// const isLoginUnique = function(allLogins, login) {
+//   return !allLogins.includes(login);
+// };
 
-// const addLogin = (allLogins, login) => {
+// const addLogin = function(allLogins, login) {
 //   if (isLoginValid(login)) {
-//     !isLoginUnique(allLogins, login) ? allLogins.push(login) && alert('Логин успешно добавлен!') :
-//     alert('Такой логин уже используется!');
+//     if(isLoginUnique(allLogins, login)) {
+//       alert('Логин успешно добавлен!');
+//       allLogins.push(login);
+//     } else {
+//       alert('Такой логин уже используется!');
+//     }
 //   } else {
 //     alert('Ошибка! Логин должен быть от 4 до 16 символов');
 //   }
@@ -50,4 +48,3 @@ console.log(logins);
 
 // addLogin(logins, userInput);
 // console.log(logins);
-
