854a73267f564f1d96b2f158905e1d02f9c5d24c
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-08-12 17:14:58 +0300

    code changes

---
+++ javascript/module-10/src/js/utils/notepad.js
@@ -0,0 +1,63 @@
+import {PRIORITY_TYPES} from './constants';
+import shortid from 'shortid';
+
+export default class Notepad {
+  constructor(notes) {
+    this._notes = notes;
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  saveUserInput(userTitle, userBody) {
+    const newItem = {
+      id: shortid.generate(),
+      title: userTitle,
+      body: userBody,
+      priority: PRIORITY_TYPES.LOW,
+    }
+    return this.saveNote(newItem);
+  }
+
+  saveNote(note) {
+    this._notes.push(note);
+    return note;
+  }
+
+  findNoteById(id) {
+    return this._notes.find(note => note.id === id);
+  }
+
+  deleteNote(id) {
+    this._notes = this._notes.filter(note => note.id !== id);
+  }
+
+  updateNoteContent(id, updatedContent) {
+    let noteToUpdate = this.findNoteById(id);
+    const updatedNote = {...noteToUpdate, ...updatedContent};
+    this._notes.splice(this._notes.indexOf(noteToUpdate), 1, updatedNote);
+    return updatedNote;
+  }
+
+  updateNotePriority(id, priority) {
+    let noteToChangePriority = this.findNoteById(id);
+    noteToChangePriority.priority = priority;
+    return noteToChangePriority;
+  }
+
+  filterNotesByQuery(query = '') {
+    return this._notes.filter(note => note.title.toLowerCase().includes(query.toLowerCase()) ||
+    note.body.toLowerCase().includes(query.toLowerCase()));
+  }
+
+  filterNotesByPriority(priority) {
+    let searchResult = [];
+    for (let key of this._notes) {
+      if (key.priority === priority) {
+        searchResult.push(key);
+      }
+    }
+    return searchResult;
+  }
+}
