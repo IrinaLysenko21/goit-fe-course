ff9078e7d07ead15a8373621719ed338fa9e3403
IrinaLysenko21 <irina.lysenko21@gmail.com>
2019-08-26 11:11:01 +0300

    saving user input in editor is done

--- javascript/module-12/src/js/utils/app.js
+++ javascript/module-12/src/js/utils/app.js
@@ -5,6 +5,7 @@ import Notepad from './notepad';
 import {renderNotesList, renderFilteredNotes, addListItem, findParentListItem, removeListItem, getRefs} from './view';
 import successMsg from '../components/Success/Success';
 import errorMsg from '../components/Error/Error'
+import storage from './storage';
 
 const notepad = new Notepad(initialNotes);
 const refs = getRefs();
@@ -13,7 +14,23 @@ renderNotesList(refs.noteList, notepad.notes);
 
 const handleOpenEditorModal = () => {
   Micromodal.show('note-editor-modal');
-  
+
+  const noteTitle = storage.load('noteTitle');
+  const noteBody = storage.load('noteBody');
+
+  if (noteTitle || noteBody) {
+    const [input, textarea] = refs.editor.elements;
+
+    input.value = noteTitle;
+    textarea.value = noteBody;
+  }
+};
+
+const handleEditorInputSaving = (evt) => {
+  const [input, textarea] = evt.currentTarget.elements;
+
+  storage.save('noteTitle', input.value);
+  storage.save('noteBody', textarea.value);
 };
 
 const handleEditorSubmit = evt => {
@@ -27,10 +44,14 @@ const handleEditorSubmit = evt => {
 
   const noteTitle = input.value;
   const noteBody = textarea.value;
+
   notepad.saveUserInput(noteTitle, noteBody)
   .then(note => addListItem(refs.noteList, note))
   .then(successMsg('Заметка успешно добавлена!'))
   .catch(console.error());
+
+  storage.remove('noteTitle');
+  storage.remove('noteBody');
 
   evt.currentTarget.reset();
   Micromodal.close('note-editor-modal');
@@ -70,5 +91,6 @@ const handleFilterInput = ({target}) => {
 
 refs.openEditorModalBtn.addEventListener('click', handleOpenEditorModal);
 refs.editor.addEventListener('submit', handleEditorSubmit);
+refs.editor.addEventListener('keyup', handleEditorInputSaving);
 refs.noteList.addEventListener('click', handleNoteClick);
 refs.searchInput.addEventListener('input', handleFilterInput);
